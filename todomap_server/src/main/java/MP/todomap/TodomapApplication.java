5bb22f0ead9413f0d3e563e41044877831988a1a
LBC11 <wpdltm4@gmail.com>
2022-11-14 10:40:53 +0900

    server, start

---
+++ todomap_server/src/main/java/MP/todomap/TodomapApplication.java
@@ -0,0 +1,13 @@
+package MP.todomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomapApplication.class, args);
+	}
+
+}
---
+++ todomap_server/src/test/java/MP/todomap/TodomapApplicationTests.java
@@ -0,0 +1,13 @@
+package MP.todomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
